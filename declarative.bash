e5fab48628cd7975dc1a0cd3b914ea0912409225
Charles Duffy <charles@threatgrid.com>
2014-03-11 16:18:31 -0500

    Add support for assertions

--- declarative.bash
+++ declarative.bash
@@ -20,11 +20,48 @@ declare -A _declarative_providees
 declare -A _declarative_completed_funcs
 declare -A _declarative_completed_vars
 
+declare -A _declarative_assertions_for_var
+
 declare -A _declarative_leak_test_data
 
 # callback; override me to change behavior
 _declarative_leak_detected() {
     echo "ERROR: Leak detected: function $1 leaks definition $2" >&2
+}
+
+_declarative_assertion_failed() {
+    echo "ERROR: Assertion $1 about variable $2 failed after invoking $3 with status $4" >&2
+    exit 1
+}
+
+_declarative_run_assertions_for_var() {
+    local func_name var_name assertion assertion_retval
+    local -a assertions_to_run
+    var_name=$1
+    func_name=$2
+    eval "assertions_to_run=( ${_declarative_assertions_for_var[$var_name]} )"
+    for assertion in "${assertions_to_run[@]}"; do
+        if [[ $DECLARATIVE_TEST_LEAKS ]]; then
+            _declarative_leak_test "$assertion" "$var_name"
+        else
+            "$assertion" "$var_name"
+        fi
+        assertion_retval=$?
+        if (( assertion_retval != 0 )); then
+            _declarative_assertion_failed "$assertion" "$var_name" "$func_name" "$assertion_retval"
+        fi
+    done
+}
+
+_declarative_run_assertions_for_func() {
+    local func_name var_name
+    local -a vars_to_check
+    for func_name; do
+        eval "vars_to_check=( ${_declarative_providees[$func_name]} )"
+        for var_name in "${vars_to_check[@]}"; do
+            _declarative_run_assertions_for_var "$var_name" "$func_name"
+        done
+    done
 }
 
 _declarative_leak_test() {
@@ -68,8 +105,9 @@ provides() {
 }
 
 needs() {
-    local var_name func_name
+    local var_name func_name retval
     local -a pending_funcs
+    retval=0
     for var_name; do
         : "var_name=$var_name"
         [[ ${_declarative_completed_vars[$var_name]} ]] && continue
@@ -84,6 +122,33 @@ needs() {
             else
                 "$func_name"
             fi
+            (( retval |= $? ))
+            _declarative_run_assertions_for_func "$func_name"
         done
     done
+    return "$retval"
 }
+
+assert_var() {
+    local var_name=$1 assertion_name=$2
+    local quoted_assertion_name
+    printf -v quoted_assertion_name '%q' "$assertion_name"
+    for assertion_name; do
+        : assertion_name="$assertion_name"
+        _declarative_assertions_for_var[$var_name]+=" $quoted_assertion_name"
+    done
+}
+
+provides() {
+    local func_name=$1
+    local var_name
+    local quoted_func_name
+    printf -v quoted_func_name '%q' "$func_name"
+
+    for var_name; do
+        : "var_name=$var_name"
+        _declarative_providers[$var_name]+=" $quoted_func_name"
+    done
+    printf -v "_declarative_providees[$func_name]" '%q ' "$@"
+}
+
